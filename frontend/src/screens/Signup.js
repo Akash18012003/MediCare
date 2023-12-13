7c60d82035152b943bbfcc4d8250a1a124386bdb
Akash18012003 <90127503+Akash18012003@users.noreply.github.com>
2023-12-13 16:46:06 +0530

    Update Signup.js

--- frontend/src/screens/Signup.js
+++ frontend/src/screens/Signup.js
@@ -10,7 +10,7 @@ export default function Signup() {
   const handleSubmit = async (e) => {
         e.preventDefault();
         
-        const response = await fetch('http://localhost:5000/api/creatuser', {
+        const response = await fetch('https://medicareserver.vercel.app/api/creatuser', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
